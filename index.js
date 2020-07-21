769412bc7451d2662ddb198cc6f9b7bf31ebd64a
communal-documentation-0347 <scottkoons@gmail.com>
2020-07-21 03:44:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
-console.log(`Happy holidays, ${happyHolidaysTo(name)}`);
+console.log(`Happy holidays, ${name}!`);
